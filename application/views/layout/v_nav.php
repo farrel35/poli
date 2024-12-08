dcadcb66b225a43499199c67c259e8beafa2f7f2
Farrel Ardian <farrelardian123@gmail.com>
2024-12-08 18:19:55 +0700

    Update v_nav.php

--- application/views/layout/v_nav.php
+++ application/views/layout/v_nav.php
@@ -163,7 +163,10 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#"><?= $menu ?></a></li>
+                        <li class="breadcrumb-item"><a
+                                href="<?= base_url() . $this->uri->segment(1) ?>"><?= $menu ?></a></li>
+
+                        </li>
                         <li class="breadcrumb-item active"><?= $title ?></li>
                     </ol>
                 </div>
