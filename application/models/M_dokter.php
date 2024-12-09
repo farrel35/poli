157699be95b27619fb713b4237dd7c035d9de45a
Farrel Ardian <farrelardian123@gmail.com>
2024-12-09 22:14:34 +0700

    Update M_dokter.php

--- application/models/M_dokter.php
+++ application/models/M_dokter.php
@@ -52,11 +52,12 @@ class M_dokter extends CI_Model
 
     public function get_daftar_poli_by_dokter($id_dokter)
     {
-        $this->db->select('tbl_daftar_poli.id, tbl_daftar_poli.keluhan, tbl_daftar_poli.no_antrian, tbl_pasien.nama as nama_pasien');
+        $this->db->select('tbl_daftar_poli.id, tbl_daftar_poli.keluhan, tbl_daftar_poli.no_antrian, tbl_pasien.nama as nama_pasien, tbl_periksa.tgl_periksa, tbl_periksa.catatan, tbl_periksa.biaya_periksa');
         $this->db->join('tbl_pasien', 'tbl_pasien.id = tbl_daftar_poli.id_pasien');
         $this->db->join('tbl_jadwal_periksa', 'tbl_jadwal_periksa.id = tbl_daftar_poli.id_jadwal');
         $this->db->join('tbl_dokter', 'tbl_dokter.id = tbl_jadwal_periksa.id_dokter');
         $this->db->join('tbl_poli', 'tbl_poli.id = tbl_dokter.id_poli');
+        $this->db->join('tbl_periksa', 'tbl_periksa.id_daftar_poli = tbl_daftar_poli.id', 'left'); // Added LEFT JOIN
         $this->db->where('tbl_dokter.id', $id_dokter);
         $query = $this->db->get('tbl_daftar_poli');
         return $query->result();
@@ -70,5 +71,26 @@ class M_dokter extends CI_Model
         $query = $this->db->get();
 
         return $query->row();
+    }
+
+    public function get_detail_periksa($id_periksa)
+    {
+        $this->db->select('
+            tbl_periksa.id AS periksa_id,
+            tbl_periksa.tgl_periksa,
+            tbl_periksa.catatan,
+            tbl_periksa.biaya_periksa,
+            tbl_detail_periksa.id AS detail_periksa_id,
+            tbl_detail_periksa.id_obat,
+            tbl_obat.nama_obat AS nama_obat,
+            tbl_obat.harga AS harga_obat
+        ');
+        $this->db->from('tbl_detail_periksa');
+        $this->db->join('tbl_periksa', 'tbl_periksa.id = tbl_detail_periksa.id_periksa', 'left');
+        $this->db->join('tbl_obat', 'tbl_detail_periksa.id_obat = tbl_obat.id', 'left');
+        $this->db->where('tbl_detail_periksa.id_periksa', $id_periksa);
+        $query = $this->db->get();
+
+        return $query->result();
     }
 }
