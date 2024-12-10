6981c0d1277303ff9b98e81823609ae3d615298f
Farrel Ardian <farrelardian123@gmail.com>
2024-12-10 22:46:58 +0700

    Update v_nav.php

--- application/views/layout/v_nav.php
+++ application/views/layout/v_nav.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?= base_url() ?>" class="brand-link">
+    <a href="#" class="brand-link">
         <img src="<?= base_url() ?>template/dist/img/logo-udinus.png" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Poliklinik</span>
